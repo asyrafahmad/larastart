6ce9f9cfc11b293bcc47847d14761d6a163eec8e
Asyraf Ahmad <asyraf@time.com.my>
2021-02-19 11:46:02 +0800

    [Add] logout route function

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -108,10 +108,14 @@
             </li>
 
             <li class="nav-item">
-                <router-link to="#" class="nav-link">
+                <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                     <i class="nav-icon fas fa-power-off"></i>
-                    <p>Log out</p>
-                </router-link>
+                    <p>{{ __('Logout') }}</p>
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
             </li>
 {{--
 
